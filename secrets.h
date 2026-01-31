27a18e79fd37f10fa7ca76cbd397e686c5471f36
olsen <olsen@enerum.tapmuk.se>
2026-01-31 19:59:35 +0100

    Duplicated code moved to function NB-IoT modem setup moved from Arduino setup MQTT reconnect limited to x retries

---
+++ secrets.h
@@ -0,0 +1,9 @@
+// For nb-iot
+#define APN                 "your.operator.apn"
+#define BAND                3
+
+// MQTT Setting
+#define MQTT_BROKER         "your.mqtt.brooker"
+#define MQTT_PORT           portnumber
+#define MQTT_USERNAME       ""
+#define MQTT_PASSWORD       ""
